49cfb344e5e957df932e4803c1f0f946e3bb9210
songkho <songkho12@naver.com>
2020-01-02 00:03:23 +0900

    gdg

--- app/src/main/java/com/idh/songk/danang/MainActivity.java
+++ app/src/main/java/com/idh/songk/danang/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private InterstitialAd mInterstitialAd;
 
+    private InterstitialAd mInterstitialAd2;
 
 
 
